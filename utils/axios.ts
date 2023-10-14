8d50c4bbe8ba4cb32c15b9a392b73a2c2c560202
Gonza Lopez <gonzalo@digitalf2f.com>
2023-10-14 16:53:53 -0300

    feat: :sparkles: add newsletter subscribe form

---
+++ utils/axios.ts
@@ -0,0 +1,28 @@
+import Axios from "axios";
+import Cookies from "js-cookie";
+
+const access_token = Cookies.get("starlings:access_token");
+
+const headers: any = {
+    "X-Requested-With": "XMLHttpRequest",
+};
+
+if (access_token) headers["Authorization"] = `Bearer ${access_token}`;
+
+const axios = Axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers,
+    withCredentials: true,
+});
+
+const onFulfilled = (response: any) => {
+    return response;
+};
+
+const onRejected = (error: any) => {
+    return Promise.reject(error);
+};
+
+axios.interceptors.response.use(onFulfilled, onRejected);
+
+export default axios;
